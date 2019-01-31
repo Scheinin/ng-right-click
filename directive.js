d8c1f7c3cb1a55611aaef9f9162f63b7405760da
scheinin <scheininxie@vectorbuilder.net>
2019-01-31 10:19:49 +0800

    directive

---
+++ directive.js
@@ -0,0 +1,14 @@
+/**
+ * Created by scheinin on 1/31/19.
+ */
+App.directive('ngRightClick', function($parse) {
+    return function(scope, element, attrs) {
+        var fn = $parse(attrs.ngRightClick);
+        element.bind('contextmenu', function(event) {
+            scope.$apply(function() {
+                event.preventDefault();
+                fn(scope, {$event:event});
+            });
+        });
+    };
+});
